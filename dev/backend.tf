75d2537a1b9183ed11e695818419bc1cd5cd8309
Birtanay <ay.birtan@gmail.com>
2019-10-16 16:45:04 +0100

    State control added

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,8 @@
 terraform {
     backend "s3" {
         bucket = "webserverterraformbay"
+        dynamodb_table = "terraform-state-lock-dynamo"
+        region = "eu-central-1"
         key = "terraformwebserverstate/dev"
     }
 }
