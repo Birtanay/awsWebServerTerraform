a8b29c97ea12255de1fc84400b6ae759a5d470e0
Birtanay <ay.birtan@gmail.com>
2019-10-17 14:22:27 +0100

    Removed VPC and Subnet Config

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "webserver" {
     ami = "${var.ami_id}"
     instance_type = "${var.instance_type}"
-    subnet_id = "${var.subnet_id}"
 
     tags = {
         Name = "WebServer"
