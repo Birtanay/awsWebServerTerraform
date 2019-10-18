4e9fbab34172c271a11a4b11a9889d195cfb3f5c
Birtanay <ay.birtan@gmail.com>
2019-10-18 15:12:49 +0100

    Security group correction

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "webserver" {
     ami = "${var.ami_id}"
     instance_type = "${var.instance_type}"
-
+    security_groups = [
+        "${aws_security_group.allow_all.id}"
+    ]
     tags = {
         Name = "WebServer"
         Environment = "Development"
